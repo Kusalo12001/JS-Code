43257b9f562109275a080473451891f56a748543
Kusal pal <110298805+Kusalo12001@users.noreply.github.com>
2025-06-23 03:33:16 +0000

    variable & datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+//JS enging are already present in browser 
+//alert(3+2)// we are using nodejs, not browser
+
+//number
+let age =22
+console.log("age: ", age, "type: ", typeof(age));
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>value not asinge
+//symbol =>unique
+
+//non premative data type
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
---
+++ 01_basic/variable.js
@@ -0,0 +1,12 @@
+const userId=13426
+let userEmail="kusal@gmail.com"
+var userPassword=12345
+userCity="Arambagh"
+
+//userId=86463 const can't re-declared or update
+userEmail="kusal123@gmail.com" //let can't re-declared but  update
+userPassword=130849  //var re-declared or  update
+userCity="Kolkata"
+
+//console.log(userId);
+console.table([userId,userEmail,userPassword,userCity])
