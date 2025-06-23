7a1891762f655153f78f4099fecefd716b07a9bb
kusal <palkusal833@gmail.com>
2025-06-23 22:38:43 +0530

    datatype & comprison

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,19 +1,45 @@
 "use strict"; //treat all JS code as newer version
-//JS enging are already present in browser 
+//JS enging are already present in browser
 //alert(3+2)// we are using nodejs, not browser
 
-//number
-let age =22
-console.log("age: ", age, "type: ", typeof(age));
+//kis tarika sa data ko memory may rak sakta ha or access kar sakta ha ush ka basis par 2 type ha
+
+//1. Premative
+// premative datatype 7 type: String, Number, Boolean, null =>  it's empty  not zero, Bigint, undefined,Symbol
+
+let age = 22;
+//console.log("age: ", age, "type: ", typeof age);
 //bigint
 //string => ""
 //boolean => true/false
 //null => standalone value
 //undefined =>value not asinge
 //symbol =>unique
+const userEmail = "kusal@gmail.com";
+const isLogIn = true;
+const temprature = null;
+let city;
+const id = Symbol("123");
+const userId = Symbol("123");
+console.log(id == userId); //false because values are same but in symbol it's unic
+const bigNumber = 124647533848748n;
+console.log(typeof bigNumber);
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
 
-//non premative data type
-//object
+//2. Reference (Non Premative)
+//Array , Object, Function
+const gods = ["mahadev", "krishna", " jaganath"]; //array
 
-console.log(typeof undefined) // undefined
-console.log(typeof null) //object
+let god = {
+  name: "krishna",
+  place: "brindhaban",
+  work: " right driction provied",
+  age: "Infinity",
+}; //object
+
+let myFunction = function () {
+  console.log("hello world");
+};
+
+//link : https://262.ecma-international.org/5.1/#sec-11.4.3
---
+++ 01_basic/04_comprison.js
@@ -0,0 +1,7 @@
+// console.log("2">1);
+// console.log("2"==2); //  true => chack value
+// console.log("2"===2); // false => chack value and datatype
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
